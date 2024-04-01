f7ca7efdf0234b1feade775586045ab438b12cb1
Caleb Carlson <ccarlson355@gmail.com>
2024-04-01 11:40:25 -0600

    Being simple sockets example

---
+++ src/socket_client.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+void print_usage() {
+        printf("Usage:\n\t./client <server_host> <server_port>\n");
+        printf("Example:\n\t./client 10.214.131.9 8082\n");
+}
+
+int main(int argc, char** argv) {
+
+        if (argc < 2) {
+                print_usage();
+                return 1;
+        }
+
+        for (int i = 0; i < argc; i++) {
+                printf("argv[%d]=%s\n", i, argv[i]);
+        }
+
+        int server_port = atoi(argv[2]);
+        const char *server_host = argv[1];
+        struct sockaddr_in server_addr;
+
+        /* Zero out server's sockaddr_in struct, and populate it */
+        memset(&server_addr, 0, sizeof(server_addr));
+
+        /* Create a socket file descriptor */
+        //int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+        /* Restricts the returned addresses to IPv4 TCP sockets */
+        struct addrinfo hints;
+        memset(&hints, 0, sizeof(hints));
+        hints.ai_family = AF_INET;
+        hints.ai_socktype = SOCK_STREAM;
+
+        struct addrinfo *current, *result;
+        void *ptr;  // Stores the cast sockaddr_in->sin_addr field of a result
+
+        /* Get a linked-list of server addrinfo objects based on hints' criteria
+         * and server host name.
+         */
+        getaddrinfo(server_host, NULL, &hints, &result);
+
+        /* Iterate over linked-list and check information */
+        current = result;
+        char addrstr[256];
+        while (current) {
+                if (current->ai_family == AF_INET) {
+                        ptr = &((struct sockaddr_in *) current->ai_addr)
+                                        ->sin_addr;
+                        inet_ntop(current->ai_family, ptr, addrstr, 256);
+                        printf ("IPv4 address: %s (%s)\n", addrstr, 
+                                current->ai_canonname);
+                }
+                current = current->ai_next;
+        }
+
+        /* Free our addrinfo linked-list of results */
+        freeaddrinfo(result);
+
+        return 0;
+}
---
+++ src/socket_server.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+
+void print_usage() {
+        printf("Usage:\n\t./server <listen_port>\n");
+        printf("Example:\n\t./server 8082\n");
+}
+
+
+int main(int argc, char** argv) {
+
+        if (argc < 2) {
+                print_usage();
+                return 1;
+        }
+
+        for (int i = 0; i < argc; i++) {
+                printf("argv[%d]=%s\n", i, argv[i]);
+        }
+
+        int server_port = atoi(argv[1]);
+        int max_client_connections = 64;
+
+        /* Stores internet address information */
+        struct sockaddr_in server_addr, client_addr;
+
+        /* Zero out server's sockaddr_in struct, and populate it */
+        memset(&server_addr, 0, sizeof(server_addr));
+        server_addr.sin_family = AF_INET;           // IPv4 address family
+        server_addr.sin_addr.s_addr = INADDR_ANY;   // Listen on 0.0.0.0
+        server_addr.sin_port = htons(server_port);  // Use network byte order
+
+        /* Create a socket file descriptor */
+        int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+        /* Bind to that socket */
+        bind(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr));
+
+        /* Listen to the socket */
+        listen(sockfd, max_client_connections);
+
+        /* Extract a connection request from the queue of pending connections */
+        socklen_t cli_addr_len = sizeof(client_addr);
+        int client_sockfd = accept(sockfd, (struct sockaddr *) &client_addr,
+                                   &cli_addr_len);
+
+        /* Allocate a static buffer and to read into */
+        char buffer[256] = { '\0' };
+
+        /* Read data from the client's sockfd into the buffer */
+        int n = read(client_sockfd, &buffer, 255);
+
+        /* Print buffer's contents */
+        printf("Client: %s\n", buffer);
+
+        /* Close client socket file descriptor */
+        close(client_sockfd);
+
+        /* Close listen socket file descriptor */
+        close(sockfd);
+
+        return 0;
+}
