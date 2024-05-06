a46110638984dc5957b8fb2655e6c21cff60ca8e
Caleb Carlson <ccarlson355@gmail.com>
2024-05-06 13:33:42 -0600

    Start working on RDMA includes

---
+++ src/rdma/rdma_server.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <getopt.h>
+#include <netdb.h>
+#include <rdma/rdma_cm.h>
+
+int main(int argc, char **argv) {
+
+        printf("Just checking includes\n");
+        return 0;
+}
