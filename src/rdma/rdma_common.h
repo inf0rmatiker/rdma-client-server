1f8923f544dd818070a05df7017957abda5c2440
Caleb Carlson <ccarlson355@gmail.com>
2024-05-09 17:51:01 -0600

    Testing

--- src/rdma/rdma_common.h
+++ src/rdma/rdma_common.h
@@ -15,6 +15,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <rdma/rdma_cma.h>
+#include <rdma/rdma_verbs.h>
 
 /*
  * process_rdma_event fully checks and processes an RDMA event on the
