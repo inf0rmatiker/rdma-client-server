07a1f178579286cf2e0fd8c90a24d4e7e6425a68
Caleb Carlson <ccarlson355@gmail.com>
2024-05-28 18:40:48 -0600

    Debugging RDMA addrinfo

--- src/rdma/rdma_common.h
+++ src/rdma/rdma_common.h
@@ -18,13 +18,33 @@
 #include <rdma/rdma_verbs.h>
 
 /*
+ * Mapping between a bitflag and its enum string.
+ */
+struct flag_str {
+        int value;
+        const char* str;
+};
+
+/*
+ * Converts a set of bitflags to a human-readable string.
+ * If there are more than 1 flags set, they are separated by the '|' character.
+ * Stores the string result in *res pointer passed in.
+ * Example: "RAI_PASSIVE | RAI_NUMERICHOST"
+ */
+void bitflags_to_str(struct flag_str *pairs, size_t count, int flags, char *res);
+
+/*
+ * Prints an rdma_addrinfo struct in human-readable terms.
+ */
+void print_rdma_addrinfo(const struct rdma_addrinfo* rai);
+
+/*
  * process_rdma_event fully checks and processes an RDMA event on the
  * Connection Manager event channel.
  */
 int process_rdma_event(struct rdma_event_channel *ec,
                        struct rdma_cm_event **event,
                        enum rdma_cm_event_type type);
-
 
 
 #endif /* RDMA_COMMON_H */
--- src/rdma/rdma_server.c
+++ src/rdma/rdma_server.c
@@ -1,9 +1,12 @@
 #include "rdma_common.h"
 
+static char *server_addr = "127.0.0.1";
+static char *server_port = "7471";
+
 /* Connection manager data structures for server */
 static struct rdma_event_channel *cm_event_channel;
 static struct rdma_cm_id *cm_server_id, *cm_client_id;
-static struct rdma_addrinfo *res, hints;
+static struct rdma_addrinfo *rai, hints;
 
 void print_usage()
 {
@@ -21,24 +24,49 @@ void cleanup_server()
 
         /* Clean-up and destroy CM event channel */
         rdma_destroy_event_channel(cm_event_channel);
-        rdma_freeaddrinfo(res);
+        rdma_freeaddrinfo(rai);
 	printf("Successfully destroyed CM event channel\n");
 }
 
 int run()
 {
+        int ret = 0;
 
+        /* Create CM event channel for asynchronous communication events */
+        cm_event_channel = rdma_create_event_channel();
+	if (!cm_event_channel) {
+                fprintf(stderr, "Creating CM event channel failed with errno: (%s)\n",
+                                strerror(errno));
+		return -errno;
+	}
+        printf("RDMA CM event channel is created successfully at %p\n",
+	       cm_event_channel);
+
+        /* Create connection identifier for the RDMA connection */
+        struct rdma_cm_id *cm_server_id = NULL;
+        ret = rdma_create_id(cm_event_channel, &cm_server_id, NULL, RDMA_PS_TCP);
+        if (ret == -1) {
+                fprintf(stderr, "Creating CM id failed with errno: (%s)\n",
+                                strerror(errno));
+		return -errno;
+        }
+        printf("Server CM id is created\n");
+
+        /* Figure out the rdma_addrinfo of our RDMA device */
         memset(&hints, 0, sizeof(hints));
         hints.ai_flags = RAI_NUMERICHOST | RAI_PASSIVE;
-        hints.ai_port_space = RDMA_PS_IPOIB;
-        int ret = rdma_getaddrinfo("192.168.0.106", "20021", &hints, &res);
+        hints.ai_port_space = RDMA_PS_TCP;
+        ret = rdma_getaddrinfo(server_addr, server_port, &hints, &rai);
         if (ret) {
                 fprintf(stderr, "Failed rdma_getaddrinfo with errno: (%s)\n",
                                 strerror(errno));
                 cleanup_server();
                 return -errno;
         }
+        printf("Successfully retrieved rdma_addrinfo\n");
+        print_rdma_addrinfo(rai);
 
+        /*
         struct ibv_qp_init_attr init_attr;
         struct ibv_qp_attr qp_attr;
         struct ibv_wc wc;
@@ -72,10 +100,16 @@ int run()
         }
         printf("Got request\n");
 
+
         rdma_disconnect(cm_client_id);
         rdma_destroy_ep(cm_client_id);
         rdma_destroy_ep(cm_server_id);
         rdma_freeaddrinfo(res);
+
+        */
+
+        rdma_freeaddrinfo(rai);
+        rdma_destroy_event_channel(cm_event_channel);
 
         // printf("res = rdma_addrinfo {"
         //        "ai_family=%d\n"
@@ -93,111 +127,105 @@ int run()
         return 0;
 }
 
+void print_usage()
+{
+        printf("Usage\n");
+        printf("\t./rdma-server -s <server_address> -p <server_port>\n");
+        printf("Example\n");
+        printf("\t./rdma-server -s 192.168.0.106 -p 7471\n");
+}
+
 int main(int argc, char **argv)
 {
-        if (argc < 2) {
-                print_usage();
-                return 1;
-        }
+        int option;
+        while ((option = getopt(argc, argv, "s:p:")) != -1) {
+                switch (option) {
+                        case 's':
+                                server_addr = optarg;
+                                break;
+                        case 'p':
+                                server_port = optarg;
+                                break;
+                        default:
+                                print_usage;
+                                exit(1);
+                }
 
-        const char *server_host = argv[1];
-        const char *port = argv[2];
-        int server_port = atoi(argv[2]);
-        int ret = 0;
+        }
 
         return run();
-
-        /* Create CM event channel for asynchronous communication events */
-        cm_event_channel = rdma_create_event_channel();
-	if (!cm_event_channel) {
-                fprintf(stderr, "Creating CM event channel failed with errno: (%s)\n",
-                                strerror(errno));
-		return -errno;
-	}
-        printf("RDMA CM event channel is created successfully at %p\n",
-	       cm_event_channel);
-
-        /* Create connection identifier for the RDMA connection */
-        struct rdma_cm_id *cm_server_id = NULL;
-        ret = rdma_create_id(cm_event_channel, &cm_server_id, NULL, RDMA_PS_IPOIB);
-        if (ret == -1) {
-                fprintf(stderr, "Creating CM id failed with errno: (%s)\n",
-                                strerror(errno));
-		return -errno;
-        }
-        printf("Server CM id is created\n");
 
         /* Bind to an RDMA address. To do this, we need to create a sockaddr_in,
          * fill out the fields, then cast it to a sockaddr* before passing it to
          * rdma_bind_addr.
          */
-        struct sockaddr_in server_sockaddr;
-        memset(&server_sockaddr, 0, sizeof(server_sockaddr));
-	server_sockaddr.sin_family = AF_INET; /* standard IP NET address */
-	server_sockaddr.sin_addr.s_addr = inet_addr(server_host); /* passed address */
-        server_sockaddr.sin_port = htons(server_port);
+        // struct sockaddr_in server_sockaddr;
+        // memset(&server_sockaddr, 0, sizeof(server_sockaddr));
+	// server_sockaddr.sin_family = AF_INET; /* standard IP NET address */
+	// server_sockaddr.sin_addr.s_addr = inet_addr(server_host); /* passed address */
+        // server_sockaddr.sin_port = htons(server_port);
 
-        ret = rdma_bind_addr(cm_server_id, (struct sockaddr *)&server_sockaddr);
-        if (ret == -1) {
-                fprintf(stderr, "Binding server RDMA address failed with errno: (%s)\n",
-                                strerror(errno));
-                cleanup_server();
-		return -errno;
-        }
-        printf("Successfully bound RDMA server address %s:%d\n", server_host,
-               server_port);
+        // ret = rdma_bind_addr(cm_server_id, (struct sockaddr *)&server_sockaddr);
+        // if (ret == -1) {
+        //         fprintf(stderr, "Binding server RDMA address failed with errno: (%s)\n",
+        //                         strerror(errno));
+        //         cleanup_server();
+	// 	return -errno;
+        // }
+        // printf("Successfully bound RDMA server address %s:%d\n", server_host,
+        //        server_port);
 
         /* Initiate a listen on the RDMA IP address and port.
          * This is a non-blocking call.
          */
-        ret = rdma_listen(cm_server_id, 8);
-        if (ret == -1) {
-                fprintf(stderr, "Listening for CM events failed: (%s)\n",
-                                strerror(errno));
-                cleanup_server();
-		return -errno;
-        }
-        printf("Server is listening successfully at: %s, port: %d \n",
-               inet_ntoa(server_sockaddr.sin_addr),
-	       ntohs(server_sockaddr.sin_port));
+        // ret = rdma_listen(cm_server_id, 8);
+        // if (ret == -1) {
+        //         fprintf(stderr, "Listening for CM events failed: (%s)\n",
+        //                         strerror(errno));
+        //         cleanup_server();
+	// 	return -errno;
+        // }
+        // printf("Server is listening successfully at: %s, port: %d \n",
+        //        inet_ntoa(server_sockaddr.sin_addr),
+	//        ntohs(server_sockaddr.sin_port));
 
         /* We expect the client to connect and generate a
          * RDMA_CM_EVENT_CONNECT_REQUEST. We wait (block) on the
          * connection management event channel for this event.
 	 */
-        struct rdma_cm_event *cm_event = NULL;
-        ret = process_rdma_event(cm_event_channel, &cm_event,
-                                 RDMA_CM_EVENT_CONNECT_REQUEST);
-        if (ret) {
-                fprintf(stderr, "Failed to process CM event\n");
-                cleanup_server();
-                return ret;
-        }
+        // struct rdma_cm_event *cm_event = NULL;
+        // ret = process_rdma_event(cm_event_channel, &cm_event,
+        //                          RDMA_CM_EVENT_CONNECT_REQUEST);
+        // if (ret) {
+        //         fprintf(stderr, "Failed to process CM event\n");
+        //         cleanup_server();
+        //         return ret;
+        // }
 
         /* We got the expected RDMA_CM_EVENT_CONNECT_REQUEST event */
-        printf("New CM event of type %s received\n",
-                rdma_event_str(cm_event->event));
+        // printf("New CM event of type %s received\n",
+        //         rdma_event_str(cm_event->event));
 
         /* Much like TCP connection, listening returns a new connection
          * id for a newly connected client. In the case of RDMA, this is stored
          * in the cm_event->id field. We need to save this information before
          * acknowledging the event, which also frees the struct.
 	 */
-	cm_client_id = cm_event->id;
-        ret = rdma_ack_cm_event(cm_event);
-        if (ret == -1) {
-                fprintf(stderr, "Failed to ACK CM event %s: (%s)\n",
-                                rdma_event_str(cm_event->event),
-                                strerror(errno));
-                rdma_destroy_id(cm_client_id);
-                cleanup_server();
-		return -errno;
-        }
-        printf("New RDMA connection stored at %p\n", cm_client_id);
+	// cm_client_id = cm_event->id;
+        // ret = rdma_ack_cm_event(cm_event);
+        // if (ret == -1) {
+        //         fprintf(stderr, "Failed to ACK CM event %s: (%s)\n",
+        //                         rdma_event_str(cm_event->event),
+        //                         strerror(errno));
+        //         rdma_destroy_id(cm_client_id);
+        //         cleanup_server();
+	// 	return -errno;
+        // }
+        // printf("New RDMA connection stored at %p\n", cm_client_id);
 
         /* Cleanup before exiting */
-        printf("Cleaning up server and exiting\n");
-        rdma_destroy_id(cm_client_id);
-        cleanup_server();
-        return 0;
+        // printf("Cleaning up server and exiting\n");
+        // rdma_destroy_id(cm_client_id);
+        // cleanup_server();
+        // return 0;
 }
