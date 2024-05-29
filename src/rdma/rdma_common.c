f7b9239710bcd94afc6c74ab5db0bbabebfadf89
Caleb Carlson <ccarlson355@gmail.com>
2024-05-29 16:42:12 -0600

    Add better printing to rdma_addrinfo

--- src/rdma/rdma_common.c
+++ src/rdma/rdma_common.c
@@ -55,15 +55,53 @@ void print_rdma_addrinfo(const struct rdma_addrinfo* rai)
 {
         printf("rdma_addrinfo{\n");
 
-        struct flag_str ai_flag_pairs[] = {
+        struct flag_str ai_flags[] = {
                 {RAI_PASSIVE, "RAI_PASSIVE"},
                 {RAI_NUMERICHOST, "RAI_NUMERICHOST"},
                 {RAI_NOROUTE, "RAI_NOROUTE"},
                 {RAI_FAMILY, "RAI_FAMILY"}
         };
-
         char ai_flags_str[64] = { 0 };
-        bitflags_to_str(ai_flag_pairs, 4, rai->ai_flags, ai_flags_str);
+        bitflags_to_str(ai_flags, 4, rai->ai_flags, ai_flags_str);
         printf("\tai_flags: %s\n", ai_flags_str);
+
+        struct flag_str ai_families[] = {
+                {AF_INET, "AF_INET"},
+                {AF_INET6, "AF_INET6"},
+                {AF_IB, "AF_IB"}
+        };
+        char ai_family_str[64] = { 0 };
+        bitflags_to_str(ai_families, 3, rai->ai_family, ai_family_str);
+        printf("\tai_family: %s\n", ai_family_str);
+
+        struct flag_str ai_qp_types[] = {
+                {IBV_QPT_UD, "IBV_QPT_UD"},
+                {IBV_QPT_RC, "IBV_QPT_RC"}
+        };
+        char ai_qp_types_str[32] = { 0 };
+        bitflags_to_str(ai_qp_types, 3, rai->ai_qp_type, ai_qp_types_str);
+        printf("\tai_qp_type: %s\n", ai_qp_types_str);
+
+        struct flag_str ai_ps_types[] = {
+                {RDMA_PS_UDP, "RDMA_PS_UDP"},
+                {RDMA_PS_IPOIB, "RDMA_PS_IPOIB"},
+                {RDMA_PS_TCP, "RDMA_PS_TCP"},
+                {RDMA_PS_IB, "RDMA_PS_IB"}
+        };
+        char rdma_ps_str[64] = { 0 };
+        bitflags_to_str(ai_ps_types, 3, rai->ai_port_space, rdma_ps_str);
+        printf("\tai_port_space: %s\n", rdma_ps_str);
+
+        printf("\tai_src_len: %d\n", rai->ai_src_len);
+        printf("\tai_dst_len: %d\n", rai->ai_dst_len);
+        printf("\tai_src_addr: { struct sockaddr* %p }\n", rai->ai_src_addr);
+        printf("\tai_dst_addr: { struct sockaddr* %p }\n", rai->ai_dst_addr);
+        printf("\tai_src_canonname: %s\n", rai->ai_src_canonname);
+        printf("\tai_dst_canonname: %s\n", rai->ai_dst_canonname);
+        printf("\tai_route_len: %d\n", rai->ai_route_len);
+        printf("\tai_route: { void* %p }\n", rai->ai_route);
+        printf("\tai_connect_len: %d\n", rai->ai_connect_len);
+        printf("\tai_connect: { void* %p }\n", rai->ai_connect);
+        printf("\tai_next: { struct rdma_addrinfo* %p }\n", rai->ai_next);
         printf("}\n");
 }
