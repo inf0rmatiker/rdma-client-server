745c26e0c3f54e193a0c4cdbe1fa51812fff8971
Caleb Carlson <ccarlson355@gmail.com>
2024-07-18 13:42:18 -0600

    Start working on client code

--- src/rdma/rdma_common.c
+++ src/rdma/rdma_common.c
@@ -53,6 +53,24 @@ void bitflags_to_str(struct flag_str *pairs, size_t count, int flags, char *res)
 
 void print_rdma_addrinfo(const struct rdma_addrinfo* rai)
 {
+        // struct rdma_addrinfo {
+        //         int			ai_flags;
+        //         int			ai_family;
+        //         int			ai_qp_type;
+        //         int			ai_port_space;
+        //         socklen_t		ai_src_len;
+        //         socklen_t		ai_dst_len;
+        //         struct sockaddr	*ai_src_addr;
+        //         struct sockaddr	*ai_dst_addr;
+        //         char			*ai_src_canonname;
+        //         char			*ai_dst_canonname;
+        //         size_t		ai_route_len;
+        //         void			*ai_route;
+        //         size_t		ai_connect_len;
+        //         void			*ai_connect;
+        //         struct rdma_addrinfo	*ai_next;
+        // };
+
         printf("rdma_addrinfo{\n");
 
         struct flag_str ai_flags[] = {
@@ -103,5 +121,176 @@ void print_rdma_addrinfo(const struct rdma_addrinfo* rai)
         printf("\tai_connect_len: %d\n", rai->ai_connect_len);
         printf("\tai_connect: { void* %p }\n", rai->ai_connect);
         printf("\tai_next: { struct rdma_addrinfo* %p }\n", rai->ai_next);
+        printf("}\n");
+}
+
+void print_ibv_device(const struct ibv_device* device)
+{
+        // struct ibv_device {
+        //         struct _ibv_device_ops	_ops;
+        //         enum ibv_node_type	        node_type;
+        //         enum ibv_transport_type	transport_type;
+        //         /* Name of underlying kernel IB device, eg "mthca0" */
+        //         char			        name[IBV_SYSFS_NAME_MAX];
+        //         /* Name of uverbs device, eg "uverbs0" */
+        //         char			        dev_name[IBV_SYSFS_NAME_MAX];
+        //         /* Path to infiniband_verbs class device in sysfs */
+        //         char			        dev_path[IBV_SYSFS_PATH_MAX];
+        //         /* Path to infiniband class device in sysfs */
+        //         char			        ibdev_path[IBV_SYSFS_PATH_MAX];
+        // };
+
+        if (!device) {
+                printf("(null)\n");
+                return;
+        }
+
+        printf("ibv_device{\n");
+
+        struct flag_str ibv_node_types[] = {
+                {IBV_NODE_UNKNOWN, "IBV_NODE_UNKNOWN"},
+                {IBV_NODE_CA, "IBV_NODE_CA"},
+                {IBV_NODE_SWITCH, "IBV_NODE_SWITCH"},
+                {IBV_NODE_ROUTER, "IBV_NODE_ROUTER"},
+                {IBV_NODE_RNIC, "IBV_NODE_RNIC"},
+                {IBV_NODE_USNIC, "IBV_NODE_USNIC"},
+                {IBV_NODE_USNIC_UDP, "IBV_NODE_USNIC_UDP"},
+                {IBV_NODE_UNSPECIFIED, "IBV_NODE_UNSPECIFIED"}
+        };
+        char node_type_str[32] = { 0 };
+        bitflags_to_str(ibv_node_types, 8, (int)device->node_type, node_type_str);
+        printf("\tnode_type: %s\n", node_type_str);
+
+        struct flag_str ibv_transport_types[] = {
+                {IBV_TRANSPORT_UNKNOWN, "IBV_TRANSPORT_UNKNOWN"},
+                {IBV_TRANSPORT_IB, "IBV_TRANSPORT_IB"},
+                {IBV_TRANSPORT_IWARP, "IBV_TRANSPORT_IWARP"},
+                {IBV_TRANSPORT_USNIC, "IBV_TRANSPORT_USNIC"},
+                {IBV_TRANSPORT_USNIC_UDP, "IBV_TRANSPORT_USNIC_UDP"}
+        };
+        char transport_type_str[32] = { 0 };
+        bitflags_to_str(ibv_transport_types, 5, (int)device->transport_type,
+                    transport_type_str);
+        printf("\transport_type: %s\n", transport_type_str);
+
+        printf("\tname: %s\n", device->name);
+        printf("\tdev_name: %s\n", device->dev_name);
+        printf("\tdev_path: %s\n", device->dev_path);
+        printf("\tibdev_path: %s\n", device->ibdev_path);
+        printf("}\n");
+}
+
+void print_rdma_cm_id(const struct rdma_cm_id* cm_id)
+{
+        // struct rdma_cm_id {
+        //         struct ibv_context	        *verbs;
+        //         struct rdma_event_channel    *channel;
+        //         void			        *context;
+        //         struct ibv_qp		*qp;
+        //         struct rdma_route	        route;
+        //         enum rdma_port_space	        ps;
+        //         uint8_t			port_num;
+        //         struct rdma_cm_event	        *event;
+        //         struct ibv_comp_channel      *send_cq_channel;
+        //         struct ibv_cq		*send_cq;
+        //         struct ibv_comp_channel      *recv_cq_channel;
+        //         struct ibv_cq		*recv_cq;
+        //         struct ibv_srq		*srq;
+        //         struct ibv_pd		*pd;
+        //         enum ibv_qp_type	        qp_type;
+        // };
+
+        if(!cm_id){
+		printf("(null)\n");
+		return;
+	}
+
+	printf("rdma_cm_id{\n");
+	if(cm_id->verbs && cm_id->verbs->device) {
+                printf("\tverbs: *ibv_context{\n\t\tdevice:\n\t");
+                print_ibv_device(cm_id->verbs->device);
+                printf("}\n");
+        }
+
+        struct flag_str rdma_ps_types[] = {
+                {RDMA_PS_IPOIB, "RDMA_PS_IPOIB"},
+                {RDMA_PS_TCP, "RDMA_PS_TCP"},
+                {RDMA_PS_UDP, "RDMA_PS_UDP"},
+                {RDMA_PS_IB, "RDMA_PS_IB"}
+        };
+        char rdma_ps_type_str[16] = { 0 };
+        bitflags_to_str(rdma_ps_types, 4, (int)cm_id->ps, rdma_ps_type_str);
+        printf("\tps: %s\n", rdma_ps_type_str);
+        printf("\tport_num: %u\n", cm_id->port_num);
+
+        struct flag_str ibv_qp_types[] = {
+                {IBV_QPT_RC, "IBV_QPT_RC"},
+                {IBV_QPT_UC, "IBV_QPT_UC"},
+                {IBV_QPT_UD, "IBV_QPT_UD"},
+                {IBV_QPT_RAW_PACKET, "IBV_QPT_RAW_PACKET"},
+                {IBV_QPT_XRC_SEND, "IBV_QPT_XRC_SEND"},
+                {IBV_QPT_XRC_RECV, "IBV_QPT_XRC_RECV"},
+                {IBV_QPT_DRIVER, "IBV_QPT_DRIVER"},
+        };
+        char ibv_qp_type_str[24] = { 0 };
+        bitflags_to_str(ibv_qp_types, 7, (int)cm_id->qp_type, ibv_qp_type_str);
+        printf("\tqp_type: %s\n", ibv_qp_type_str);
+
+	printf("}\n");
+}
+
+void print_ibv_mr(const struct ibv_mr *mr)
+{
+        // struct ibv_mr {
+        //         struct ibv_context   *context;
+        //         struct ibv_pd	*pd;
+        //         void		        *addr;
+        //         size_t		length;
+        //         uint32_t		handle;
+        //         uint32_t		lkey;
+        //         uint32_t		rkey;
+        // };
+
+        printf("ibv_mr{\n");
+        printf("\tcontext: %p\n", mr->context);
+        printf("\tpd: %p\n", mr->pd);
+        printf("\taddr: %p\n", mr->addr);
+        printf("\tlength: %u\n", mr->length);
+        printf("\thandle: %u\n", mr->handle);
+        printf("\tlkey: %u\n", mr->lkey);
+        printf("\trkey: %u\n", mr->rkey);
+        printf("}\n");
+}
+
+void print_ibv_sge(const struct ibv_sge *sge)
+{
+        // struct ibv_sge {
+        //         uint64_t	addr;
+        //         uint32_t	length;
+        //         uint32_t	lkey;
+        // };
+
+        printf("ibv_sge{\n");
+        printf("\taddr: %u\n", sge->addr);
+        printf("\tlength: %u\n", sge->length);
+        printf("\tlkey: %u\n", sge->lkey);
+        printf("}\n");
+}
+
+void print_rdma_buffer_attr(const struct rdma_buffer_attr *rba)
+{
+        // struct rdma_buffer_attr {
+        //         uint64_t address;
+        //         uint32_t length;
+        //         union stag {
+        //                 uint32_t local_stag;
+        //                 uint32_t remote_stag;
+        //         } stag;
+        // };
+
+        printf("rdma_buffer_attr{\n");
+        printf("\taddress: %u\n", rba->address);
+        printf("\tlength: %u\n", rba->length);
+        printf("\tstag: %u\n", rba->stag.local_stag);
         printf("}\n");
 }
--- src/rdma/rdma_server.c
+++ src/rdma/rdma_server.c
@@ -1,28 +1,125 @@
+/*
+ * Description:
+ *      RDMA server implementation
+ * Author:
+ *      Caleb Carlson <ccarlson355@gmail.com>
+ * References:
+ *      https://github.com/linux-rdma/rdma-core/blob/master/librdmacm/examples
+ *      https://github.com/animeshtrivedi/rdma-example
+ */
+
 #include "rdma_common.h"
 
 static char *server_addr = "127.0.0.1";
 static char *server_port = "7471";
 
-/* Connection manager data structures for server */
+/* Connection Manager data structures for server */
 static struct rdma_event_channel *cm_event_channel;
 static struct rdma_cm_id *cm_server_id, *cm_client_id;
 static struct rdma_addrinfo *rai, hints;
 
-/* Cleans up Connection Manager ID and Event Channel objects before exiting.
+/* RDMA Queue Pair and Protection Domain resources */
+static struct ibv_pd *protection_domain = NULL;
+static struct ibv_qp *client_queue_pair = NULL;
+static struct ibv_cq *completion_queue = NULL;
+static struct ibv_comp_channel *io_completion_channel = NULL;
+static struct ibv_qp_init_attr qp_init_attr;
+
+/* Memory resources */
+static struct ibv_mr *client_metadata_mr = NULL;
+static struct ibv_mr *server_metadata_mr = NULL;
+static struct ibv_mr *server_buffer_mr = NULL;
+
+/* Receive buffer to which the server will store metadata about the client */
+static struct rdma_buffer_attr client_metadata_attr;
+/* Send buffer from where client will retrieve metadata about the server */
+static struct rdma_buffer_attr server_metadata_attr;
+/* Server's send and receive scatter-gather entries (SGE) for work requests */
+static struct ibv_sge client_recv_sge, server_send_sge;
+
+/* Send and receive work requests, along with bad WR pointers */
+static struct ibv_recv_wr client_recv_wr, *bad_client_recv_wr = NULL;
+static struct ibv_send_wr server_send_wr, *bad_server_send_wr = NULL;
+
+/* Dynamically allocated and registered memory accessible by the client */
+char *server_buffer = NULL;
+
+/* Cleans up all allocated/registered resources, in reverse order that they were
+ * created, conditionally if they've been allocated or initalized.
  */
 void cleanup_server()
 {
-        /* Destroy client and server connection identifiers */
-        rdma_destroy_ep(cm_client_id);
-        rdma_destroy_ep(cm_server_id);
+        /* Destroy server memory buffer */
+        if (server_buffer) {
+                free(server_buffer);
+        }
+
+        /* Destroy client metadata buffer */
+        if (client_metadata_mr) {
+                rdma_buffer_deregister(client_metadata_mr);
+        }
+
+        /* Destroy queue pairs */
+        if (client_queue_pair) {
+                printf("Destroying queue pairs\n");
+                ibv_destroy_qp(client_queue_pair);
+        }
+
+        /* Destroy completion queue */
+        if (completion_queue) {
+                printf("Destroying completion queue\n");
+                ibv_destroy_cq(completion_queue);
+        }
+
+        /* Destroy completion channel */
+        if (io_completion_channel) {
+                printf("Destroying I/O completion channel\n");
+                ibv_destroy_comp_channel(io_completion_channel);
+        }
+
+        /* Deallocate protection domain*/
+        if (protection_domain) {
+                printf("Deallocating protection domain\n");
+                ibv_dealloc_pd(protection_domain);
+        }
+
+        /* Destroy client/server CM connection ids */
+        if (cm_client_id) {
+                printf("Freeing client CM id\n");
+                rdma_destroy_id(cm_client_id);
+        }
+        if (cm_server_id) {
+                printf("Freeing server CM id\n");
+                rdma_destroy_id(cm_server_id);
+        }
+
+        /* Clean up server RDMA addrinfo struct */
+        if (rai) {
+                printf("Freeing server rdma_addrinfo\n");
+                rdma_freeaddrinfo(rai);
+        }
 
         /* Clean-up and destroy CM event channel */
-        rdma_destroy_event_channel(cm_event_channel);
-        rdma_freeaddrinfo(rai);
-	printf("Successfully destroyed CM event channel\n");
+        if (cm_event_channel) {
+                printf("Destroying server CM event channel\n");
+                rdma_destroy_event_channel(cm_event_channel);
+        }
+	printf("Successfully cleaned up all server resources.\n");
 }
 
-int run()
+/*
+ * Sets up the initial connection resources for the server:
+ * 1. Create Connection Manager event channel
+ * 2. Create Connection Manager id for the server
+ * 3. Get RDMA address info for our RDMA device
+ * 4. Bind our RDMA device to an address
+ * 5. Set up the server to listen on that address
+ * 6. Block for an RDMA_CM_EVENT_CONNECT_REQUEST event on
+ *    the event channel, capturing the client's CM id when
+ *    we receive one.
+ * 7. Ack the event, freeing it as a result.
+ */
+int setup_server()
 {
         int ret = 0;
 
@@ -37,7 +134,6 @@ int run()
 	       cm_event_channel);
 
         /* Create connection identifier for the RDMA connection */
-        struct rdma_cm_id *cm_server_id = NULL;
         ret = rdma_create_id(cm_event_channel, &cm_server_id, NULL, RDMA_PS_TCP);
         if (ret == -1) {
                 fprintf(stderr, "Creating CM id failed with errno: (%s)\n",
@@ -45,6 +141,7 @@ int run()
 		return -errno;
         }
         printf("Server CM id is created\n");
+        print_rdma_cm_id(cm_server_id);
 
         /* Figure out the rdma_addrinfo of our RDMA device. */
         memset(&hints, 0, sizeof(hints));
@@ -66,7 +163,7 @@ int run()
                         strerror(errno));
                 return -errno;
 	}
-        printf("Successfully bound RDMA server address %s:%d\n", server_addr,
+        printf("Successfully bound RDMA server address %s:%s\n", server_addr,
                server_port);
 
         /* Initiate a listen on the RDMA IP address and port.
@@ -81,6 +178,190 @@ int run()
         printf("Server is listening successfully at: %s, port: %d \n",
                inet_ntoa(((struct sockaddr_in *)rai->ai_src_addr)->sin_addr),
 	       ntohs(((struct sockaddr_in *)rai->ai_src_addr)->sin_port));
+
+        /* We expect the client to connect and generate a
+         * RDMA_CM_EVENT_CONNECT_REQUEST. We wait (block) on the
+         * CM event channel for this event.
+	 */
+        struct rdma_cm_event *cm_event = NULL;
+        ret = process_rdma_event(cm_event_channel, &cm_event,
+                                 RDMA_CM_EVENT_CONNECT_REQUEST);
+        if (ret) {
+                fprintf(stderr, "Failed to process CM event\n");
+                return ret;
+        }
+
+        /* We got the expected RDMA_CM_EVENT_CONNECT_REQUEST event */
+        printf("New CM event of type %s received\n",
+                rdma_event_str(cm_event->event));
+
+        /* Much like TCP connection, listening returns a new connection
+         * id for a newly connected client. In the case of RDMA, this is stored
+         * in the cm_event->id field. We need to save this information before
+         * acknowledging the event, which also frees the struct.
+	 */
+	cm_client_id = cm_event->id;
+        ret = rdma_ack_cm_event(cm_event);
+        if (ret == -1) {
+                fprintf(stderr, "Failed to ACK CM event %s: (%s)\n",
+                                rdma_event_str(cm_event->event),
+                                strerror(errno));
+		return -errno;
+        }
+        printf("New RDMA connection stored at %p\n", cm_client_id);
+
+        return ret;
+}
+
+/*
+ * Establish IB Verbs communication resources, allowing
+ * us to communicate with client RDMA device:
+ * 1. Set up Protection Domain, using client's RDMA device verbs provider
+ * 2. Set up I/O completion channel, using client's RDMA device verbs provider
+ * 3. Set up a Completion Queue for Work Completion metadata
+ * 4. Request notifications for all event types on CQ
+ * 5. Create a Queue Pair using initial attributes
+ */
+int setup_communication_resources()
+{
+        int ret = 0;
+
+        /* Create a Protection Domain (PD) using the CM id we got from the
+         * client connection request earlier. That cm_id->verbs defines the
+         * verbs provider of the client's RDMA device.
+         */
+        protection_domain = ibv_alloc_pd(cm_client_id->verbs);
+        if (!protection_domain) {
+                fprintf(stderr, "Failed to create Protection Domain: %s\n",
+                        strerror(errno));
+                return -errno;
+        }
+        printf("Created Protection Domain\n");
+
+        /* Create a Completion Channel (CC) where I/O completion notifications
+         * are sent. A CC is tied to an RDMA device, so we will use
+         * cm_client_id->verbs here.
+         */
+        io_completion_channel = ibv_create_comp_channel(cm_client_id->verbs);
+        if (!io_completion_channel) {
+                fprintf(stderr, "Failed to create Completion Channel: %s\n",
+                        strerror(errno));
+                return -errno;
+        }
+        printf("Created I/O Completion Channel\n");
+
+        /* Create a Completion Queue (CQ) where actual I/O completion
+	 * metadata is placed. The metadata is packed into a
+	 * struct ibv_wc (wc = work completion). ibv_wc has detailed
+	 * information about the work completion. An I/O request in RDMA world
+	 * is called "work"
+	 */
+	completion_queue = ibv_create_cq(
+                cm_client_id->verbs /* which device */,
+		16 /* maximum capacity */,
+		NULL /* user context, not used here */,
+		io_completion_channel /* IO completion channel to use */,
+		0 /* signaling vector, not used here */
+        );
+	if (!completion_queue) {
+                fprintf(stderr, "Failed to create Completion Queue: %s\n",
+                        strerror(errno));
+		return -errno;
+	}
+	printf("Completion Queue (CQ) is created at with %d elements\n",
+	       completion_queue->cqe);
+
+        /* Ask CQ to give us all events, and not filter any */
+        ret = ibv_req_notify_cq(
+                completion_queue, /* which CQ we're requesting notifications */
+                0 /* request all event types, no filter*/
+        );
+        if (ret) {
+                fprintf(stderr, "Failed to request notifications for all event types on CQ: %s\n",
+                        strerror(errno));
+		return -errno;
+        }
+
+        /* Set up the Queue Pairs (send, receive) and their capacity.
+         * The capacity here is defined statically, but we could dynamically
+         * probe these values from the device if we wanted to.
+         *
+         * To do this, we first need to set up the QP initial requested
+         * attributes struct (ibv_qp_init_attr).
+         */
+        memset(&qp_init_attr, 0, sizeof qp_init_attr);
+        qp_init_attr.cap.max_recv_sge = 2; /* Max SGE per receive posting */
+        qp_init_attr.cap.max_send_sge = 2; /* Max SGE per send posting */
+        qp_init_attr.cap.max_recv_wr = 8;  /* Max receive posting capacity */
+        qp_init_attr.cap.max_send_wr = 8;  /* Max send posting capacity */
+        /* Use the same CQ for both send/receive completion events */
+        qp_init_attr.recv_cq = completion_queue; /* Where to notify for receive completion operations */
+        qp_init_attr.send_cq = completion_queue; /* Where to notify for send completion operations */
+
+        /* Finally, create a QP. After this call, the ibv_qp reference will be
+         * stored in the client's CM id: client_cm_id->qp.
+         */
+        ret = rdma_create_qp(
+                cm_client_id, /* Which connection id */
+                protection_domain, /* Which protection domain */
+                &qp_init_attr /* Initial QP attributes */
+        );
+        if (ret) {
+                fprintf(stderr, "Failed to create QP: %s\n",
+                        strerror(errno));
+		return -errno;
+        }
+        client_queue_pair = cm_client_id->qp;
+        printf("Created QP for client on server\n");
+
+        return ret;
+}
+
+/* Posts a receive buffer and accepts a client RDMA connection. */
+int accept_client_connections()
+{
+        int ret = 0;
+
+        /* Register the client metadata buffer as an ibv_mr */
+        client_metadata_mr = ibv_reg_mr(
+                protection_domain, /* use main protection domain */
+                &client_metadata_attr, /* the memory to register */
+                sizeof(client_metadata_attr), /* size of memory to register */
+                (IBV_ACCESS_LOCAL_WRITE) /* only allow local RDMA device to write to it*/
+        );
+        if (!client_metadata_mr) {
+                /* Most likely out-of-memory (OOM), so error would be ENOMEM */
+                fprintf(stderr, "Failed to register client metadata attributes: %s\n",
+                        strerror(errno));
+		return -errno;
+        }
+        printf("Successfully registered client metadata attributes memory buffer\n");
+        print_ibv_mr(client_metadata_mr);
+        print_rdma_buffer_attr(&client_metadata_attr);
+
+        /* Pre-post receive buffer for the client on the QP. This is creating
+         * a work request (WR) for the client that indicates where the client
+         * should place its metadata.
+         */
+	//client_recv_sge.addr = (uint64_t) client_metadata_mr->addr; // same as &client_buffer_attr
+	//client_recv_sge.length = client_metadata_mr->length;
+	//client_recv_sge.lkey = client_metadata_mr->lkey;
+	/* Link SGE to the work request */
+	//memset(&client_recv_wr, 0, sizeof(client_recv_wr));
+	//client_recv_wr.sg_list = &client_recv_sge;
+	//client_recv_wr.num_sge = 1;
+	//ret = ibv_post_recv(
+        //      client_queue_pair /* which QP */,
+	// 	&client_recv_wr /* receive work request*/,
+	// 	&bad_client_recv_wr /* error WRs */
+        // );
+
+
+        return 0;
+}
+
+int run()
+{
 
 
         /*
@@ -125,10 +406,6 @@ int run()
 
         */
 
-        printf("Freeing rdma_addrinfo\n");
-        rdma_freeaddrinfo(rai);
-        printf("Closing CM event channel\n");
-        rdma_destroy_event_channel(cm_event_channel);
         return 0;
 }
 
@@ -158,59 +435,17 @@ int main(int argc, char **argv)
 
         }
 
-        return run();
+        int ret = setup_server();
+        if (ret)
+                fprintf(stderr, "Failed to establish the server\n");
+        ret = setup_communication_resources();
+        if (ret)
+                fprintf(stderr, "Failed to establish communication resources\n");
 
-        /* Initiate a listen on the RDMA IP address and port.
-         * This is a non-blocking call.
-         */
-        // ret = rdma_listen(cm_server_id, 8);
-        // if (ret == -1) {
-        //         fprintf(stderr, "Listening for CM events failed: (%s)\n",
-        //                         strerror(errno));
-        //         cleanup_server();
-	// 	return -errno;
-        // }
-        // printf("Server is listening successfully at: %s, port: %d \n",
-        //        inet_ntoa(server_sockaddr.sin_addr),
-	//        ntohs(server_sockaddr.sin_port));
+        /* Allocate server buffer */
+        server_buffer = (char *)calloc(4096, sizeof(char));
 
-        /* We expect the client to connect and generate a
-         * RDMA_CM_EVENT_CONNECT_REQUEST. We wait (block) on the
-         * connection management event channel for this event.
-	 */
-        // struct rdma_cm_event *cm_event = NULL;
-        // ret = process_rdma_event(cm_event_channel, &cm_event,
-        //                          RDMA_CM_EVENT_CONNECT_REQUEST);
-        // if (ret) {
-        //         fprintf(stderr, "Failed to process CM event\n");
-        //         cleanup_server();
-        //         return ret;
-        // }
-
-        /* We got the expected RDMA_CM_EVENT_CONNECT_REQUEST event */
-        // printf("New CM event of type %s received\n",
-        //         rdma_event_str(cm_event->event));
-
-        /* Much like TCP connection, listening returns a new connection
-         * id for a newly connected client. In the case of RDMA, this is stored
-         * in the cm_event->id field. We need to save this information before
-         * acknowledging the event, which also frees the struct.
-	 */
-	// cm_client_id = cm_event->id;
-        // ret = rdma_ack_cm_event(cm_event);
-        // if (ret == -1) {
-        //         fprintf(stderr, "Failed to ACK CM event %s: (%s)\n",
-        //                         rdma_event_str(cm_event->event),
-        //                         strerror(errno));
-        //         rdma_destroy_id(cm_client_id);
-        //         cleanup_server();
-	// 	return -errno;
-        // }
-        // printf("New RDMA connection stored at %p\n", cm_client_id);
-
-        /* Cleanup before exiting */
-        // printf("Cleaning up server and exiting\n");
-        // rdma_destroy_id(cm_client_id);
-        // cleanup_server();
-        // return 0;
+        /* Clean up all dynamically allocated resources */
+        cleanup_server();
+        return ret;
 }
