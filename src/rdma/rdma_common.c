5b1d88524ab70823772351de0ad6490afcfae1be
Caleb Carlson <ccarlson355@gmail.com>
2024-07-31 16:19:32 -0600

    Fix bad ret status check

--- src/rdma/rdma_common.c
+++ src/rdma/rdma_common.c
@@ -8,14 +8,15 @@ int process_rdma_event(struct rdma_event_channel *event_channel,
         int ret = rdma_get_cm_event(event_channel, event);
         if (ret) {
                 fprintf(stderr, "Blocking for CM events failed: (%s)\n",
-                                strerror(errno));
+                        strerror(errno));
                 return -errno;
         }
 
         /* Check the status of the event */
         if ((*event)->status != 0) {
-                fprintf(stderr, "CM event received with non-zero status: (%d)\n",
-                                (*event)->status);
+                fprintf(stderr, "CM event %s received with non-zero status: (%d)\n",
+                        rdma_event_str((*event)->event),
+                        (*event)->status);
 
                 /* Even if we get a bad status we still need to ACK the event */
                 rdma_ack_cm_event(*event);
@@ -25,8 +26,8 @@ int process_rdma_event(struct rdma_event_channel *event_channel,
         /* Check the type of event is what we expect */
         if ((*event)->event != expected_type) {
                 fprintf(stderr, "CM event received with unexpected type. Expected %s, but got %s\n",
-                                rdma_event_str(expected_type),
-                                rdma_event_str((*event)->event));
+                        rdma_event_str(expected_type),
+                        rdma_event_str((*event)->event));
                 /* Even if we got unexpected event type we still need to ACK */
                 rdma_ack_cm_event((*event));
                 return -1;
